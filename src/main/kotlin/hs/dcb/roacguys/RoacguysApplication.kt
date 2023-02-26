7e0f999731947ffd0d0ca0e83f2da45ee1d2bb2c
히송 <hesong@hisong-ui-MacBookAir.local>
2023-02-26 16:19:36 +0900

    Initial commit

---
+++ src/main/kotlin/hs/dcb/roacguys/RoacguysApplication.kt
@@ -0,0 +1,11 @@
+package hs.dcb.roacguys
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoacguysApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoacguysApplication>(*args)
+}
---
+++ src/test/kotlin/hs/dcb/roacguys/RoacguysApplicationTests.kt
@@ -0,0 +1,13 @@
+package hs.dcb.roacguys
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoacguysApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
